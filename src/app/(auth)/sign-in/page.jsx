b12b56398c1a37a29e5ad788d37f7dfbb182930f
devanshtyagi26 <tyagidevansh3@gmail.com>
2025-06-29 21:42:28 +0530

    Sign in frontend

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -1,7 +1,114 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
 
 function SignInPage() {
-  return <div>SignIn</div>;
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+      callbackUrl: "/dashboard",
+    });
+
+    setLoading(false);
+
+    if (result?.error) {
+      toast.error("Login Failed", {
+        description: result.error || "Incorrect username or password.",
+      });
+    }
+
+    if (result?.ok && result?.url) {
+      router.replace(result.url); // Manually navigate to the callback URL
+    }
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="identifier"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email/Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Password" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            disabled={
+              loading || form.formState.isSubmitting || !form.formState.isDirty
+            }
+          >
+            {loading ? <Loader2 className="w-4 h-4 animate-spin" /> : "Sign In"}
+          </Button>
+        </form>
+      </Form>
+      <p>
+        Don't have an account? <Link href="/signup">Sign up</Link>
+      </p>
+    </div>
+  );
 }
 
 export default SignInPage;
